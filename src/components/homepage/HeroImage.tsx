3b0748c7af7ee7ccd37e1055ef63f68b52c44477
moustafaessam221 <moustafaessam991@gmail.com>
2025-04-12 17:04:22 +0200

    Fix: fix ui bugs

--- src/components/homepage/HeroImage.tsx
+++ src/components/homepage/HeroImage.tsx
@@ -41,11 +41,15 @@ export const HeroImage = () => {
   }
 
   return (
-    <div style={{ backgroundColor: selectedColorValue }} className="relative">
+    <div
+      style={{ backgroundColor: selectedColorValue }}
+      className="relative lg:min-w-[544px]"
+    >
       <img src={productImages[currentImageIndex]} alt="" />
-      <div className="flex justify-between absolute bottom-5 w-full items-center flex-wrap gap-2.5">
-        <div className="px-4 py-2.5 bg-white rounded-[100px] flex items-center gap-2.5 border border-gray-200 order-2 lg:order-1">
-          <p>Color</p>
+      <div className="flex  absolute bottom-5 w-full items-center justify-between flex-wrap px-4 gap-4 md:gap-0">
+        {/* Color selector */}
+        <div className="px-4 h-12 bg-white rounded-[100px] flex items-center gap-0.5 border border-gray-200 order-2 md:order-1 ">
+          <p className="text-[14px]">Color</p>
           <ColorSelector
             colors={productData.colors}
             selectedColor={selectedColor}
@@ -53,8 +57,8 @@ export const HeroImage = () => {
             onSelectValue={setSelectedColorValue}
           />
         </div>
-        <div className="px-4 py-2.5 bg-white rounded-[100px] flex items-center gap-2.5 border border-gray-200">
-          <p>Sizes</p>
+        <div className="px-4 h-12 bg-white rounded-[100px] flex items-center gap-1.5 border border-gray-200 order-1 md:order-2 lg:order-2 w-full md:max-w-[42%] justify-center">
+          <p className="text-[14px]">Sizes</p>
           <SizeSelector
             sizes={productData.sizes}
             selectedSize={selectedSize}
@@ -63,7 +67,7 @@ export const HeroImage = () => {
         </div>
         <Link
           to={`/products`}
-          className="px-4 py-2.5 bg-white rounded-[100px] flex items-center gap-2.5 border border-gray-200 hover:bg-gray-100 hover:cursor-pointer transition-all ease-in-out duration-150"
+          className="px-4 h-12 bg-white rounded-[100px] flex items-center gap-2.5 border border-gray-200 hover:bg-gray-100 hover:cursor-pointer transition-all ease-in-out duration-150 order-3 whitespace-nowrap text-[14px]"
         >
           View Similar <ArrowRight />
         </Link>
--- src/components/productsDetails/SizeSelector.tsx
+++ src/components/productsDetails/SizeSelector.tsx
@@ -10,15 +10,15 @@ const SizeSelector = ({
   onSelectSize,
 }: SizeSelectorProps) => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 w-full justify-between">
       {sizes.map((size) => (
         <button
           key={size}
-          className={`w-8 h-8 p-2 flex items-center justify-center border rounded text-sm transition-all  cursor-pointer
+          className={`w-full py-2 lg:px-3 lg:w-auto flex items-center justify-center border rounded-4xl text-[12px] transition-all cursor-pointer border-custom-white-95
             ${
               selectedSize === size
                 ? "bg-black text-white"
-                : "bg-white text-black hover:bg-gray-100"
+                : "bg-custom-white-97 text-black hover:bg-gray-100"
             }`}
           onClick={() => onSelectSize(size)}
         >
